6ca138fae3b3a1c166c0f742796c970925684094
Leo <leo_mitrofanov@outlook.com>
2018-12-19 21:00:02 +0200

    new

--- src/tests/HomePagesTests.java
+++ src/tests/HomePagesTests.java
@@ -1,6 +1,5 @@
 package tests;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,48 +10,38 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class HomePagesTests extends testBase {
+/**
+ * Created by Inka on 16-Dec-18.
+ */
+public class HomePagesTests extends TestBase{
     @Test
     public void openHomePage() throws InterruptedException {
 
-        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Login')]"),45);
-        List<WebElement> topLinksList = driver.findElements(By.cssSelector(".mat-button-wrapper .mat-button-wrapper"));
-        System.out.println("Login:" + topLinksList.get(0).getText());
-        System.out.println("Create Account:" + topLinksList.get(1).getText());
+        waitUntilElementIsLoaded(driver,
+                By.xpath("//span[contains(text(),'Login')]"),
+                45);
+
         WebElement goToEventsButton = driver.findElement(
                 By.className("mat-stroked-button"));
-        System.out.println("Text of button: " + goToEventsButton.getText());
-        Thread.sleep(5000);
-        Assert.assertTrue(goToEventsButton.getText().equals("Go to Event list"));  // If text on button equals text "Go to event" = true
-
+        Assert.assertTrue(goToEventsButton.getText()
+                .equals("Go to Event list"));
     }
-
-
-
     @Test
-    public void goToEventList() throws InterruptedException {
+    public void goToEventsTest() throws InterruptedException {
+        waitUntilElementIsLoaded(driver,
+                By.xpath("//span[contains(text(),'Login')]"),
+                45);
 
-        Thread.sleep(10000);
-        List<WebElement> topLinksList = driver.findElements(By.cssSelector(".mat-button-wrapper .mat-button-wrapper"));
-        System.out.println("Login:" + topLinksList.get(0).getText());
-        System.out.println("Create Account:" + topLinksList.get(1).getText());
         WebElement goToEventsButton = driver.findElement(
                 By.className("mat-stroked-button"));
-        System.out.println("Text of button: " + goToEventsButton.getText());
-        Thread.sleep(5000);
-        goToEventsButton.click();
-        Thread.sleep(5000);
-        WebElement titlePage = driver.findElement(By.xpath("//h1[@class='gorisontal-center']"));
-        Assert.assertTrue(titlePage.getText().equals("Find event"));
 
+        goToEventsButton.click();
+        waitUntilElementIsLoaded(driver,
+                By.xpath("//span[contains(text(),'Filtes')])"),
+                20);
+        WebElement titlePage = driver.findElement(By
+                .xpath("//h1[@class='gorisontal-center']"));
+        Assert.assertTrue(titlePage.getText().equals("Find event"));
     }
-    private void waitUntilElementIsLoaded(WebDriver driver, By locator, int time) {
-        try {
-            new WebDriverWait(driver, time).until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+
 }
---
+++ src/tests/LoginPageTests.java
@@ -0,0 +1,63 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginPageTests extends TestBase{
+    @Test
+    public void loginPositive() throws InterruptedException {
+
+        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Login')]"),20);
+
+        WebElement login = driver.findElement(By
+                .xpath("//span[contains(text(),'Login')]"));
+        login.click();
+        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Cancel')]"),20);
+        WebElement email_field = driver.findElement(By
+                .xpath("//input[@formcontrolname='email']"));
+        WebElement password_field = driver.findElement(By
+                .xpath("//input[@formcontrolname='password']"));
+        email_field.click();
+        email_field.sendKeys("mishUser1@gmail.com");
+        password_field.click();
+        password_field.sendKeys("example");
+        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Log in')]"),20);
+        WebElement login_button=driver.findElement(By
+                .xpath("//span[contains(text(),'Log in')]"));
+        login_button.click();
+        waitUntilElementIsLoaded(driver, By.xpath("//mat-icon[@class='but mat-icon material-icons']"),20);
+        WebElement iconMenu = driver.findElement(By.xpath("//mat-icon[@class='but mat-icon material-icons']"));
+        Assert.assertTrue(iconMenu.getAttribute("mattooltip").equals("Menu"));
+
+    }
+    @Test
+    public void loginNegative(){
+        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Login')]"),20);
+
+        WebElement login = driver.findElement(By
+                .xpath("//span[contains(text(),'Login')]"));
+        login.click();
+        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Cancel')]"),20);
+        WebElement email_field = driver.findElement(By
+                .xpath("//input[@formcontrolname='email']"));
+        WebElement password_field = driver.findElement(By
+                .xpath("//input[@formcontrolname='password']"));
+        email_field.click();
+        email_field.sendKeys("mishUser1@gmail.com");
+        password_field.click();
+        password_field.sendKeys("example1111");
+        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Log in')]"),20);
+        WebElement login_button=driver.findElement(By
+                .xpath("//span[contains(text(),'Log in')]"));
+        login_button.click();
+        waitUntilElementIsLoaded(driver, By.xpath("//div[@class='alert alert-danger ng-star-inserted']"),20);
+        WebElement alertText = driver.findElement(By.xpath("//div[@class='alert alert-danger ng-star-inserted']"));
+        Assert.assertTrue(alertText.getText().equals("Wrong authorization, login or password"));
+    }
+
+
+}
